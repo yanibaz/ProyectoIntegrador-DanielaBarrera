b74ee038fc93b5b03507cc3a8e33fbcfbc855efa
Dani-AF <danielabarrera@hotmail.com.ar>
2022-05-23 15:15:29 -0300

    Integracion de Logos y Banner

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgramaComponent } from './components/logo-arg-programa/logo-arg-programa.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgramaComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
---
+++ ProyectoIntegrador/src/app/components/logo-arg-programa/logo-arg-programa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-arg-programa',
+  templateUrl: './logo-arg-programa.component.html',
+  styleUrls: ['./logo-arg-programa.component.css']
+})
+export class LogoArgProgramaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
